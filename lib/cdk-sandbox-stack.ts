93516dc96d5441062c9462b1a6334a3e7e46ffce
Shahin <shahin3213@gmail.com>
2025-12-15 11:36:51 +0100

    Add safe sandbox S3 bucket

--- lib/cdk-sandbox-stack.ts
+++ lib/cdk-sandbox-stack.ts
@@ -6,8 +6,12 @@ export class CdkSandboxStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true
+    new s3.Bucket(this, 'SandboxBucket', {
+      bucketName: undefined, // let AWS generate a unique name
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
   }
 }
